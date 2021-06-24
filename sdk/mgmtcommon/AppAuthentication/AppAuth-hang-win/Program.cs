00e55327d625b1a4ca498142b84f9694585d24e1
Matt McLoughlin <mattmcl@microsoft.com>
2021-06-23 17:27:12 -0700

    Repro for GetAccessTokenAsync hanging on Windows domain-joined machines

---
+++ sdk/mgmtcommon/AppAuthentication/AppAuth-hang-win/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Services.AppAuthentication;
+using System;
+using System.Threading.Tasks;
+
+namespace AppAuth_hang_win
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Calling GetAccessTokenAsync...");
+
+            // this hangs forever on Windows domain-joined machines
+            var token = await new AzureServiceTokenProvider().GetAccessTokenAsync("https://management.azure.com/"); 
+            
+            Console.WriteLine($"Success.  No bug found in this configuration.  Press any key to quit.");
+            Console.ReadKey();
+        }
+    }
+}
